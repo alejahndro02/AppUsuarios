6a603d58ff01ea2a2c5506e3b76d198c38853bf5
Alejahndro <alejahndro02@gmail.com>
2021-06-27 14:17:55 -0500

    Se declaro el metodo getuser como un observable y se le pasan como argumentos la url y el token para conectarse con el api

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  url='https://gorest.co.in/public-api/users';
+
+  //Este token es de la pagina de goRest
+  token='9ab421eab276107c2ec320f9048c52b500f9a3e4d45b06c2b4572ed4d673392c';
+  constructor(private http:HttpClient) { }
+  getUsuarios():Observable<any>{
+    return this.http.get<any>(this.url + '?access-token=xxx' + this.token);
+  }
+}
