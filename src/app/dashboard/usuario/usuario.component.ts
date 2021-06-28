225c5c73d919fb644b2e133dc27e31d90738a0a4
Alejahndro <alejahndro02@gmail.com>
2021-06-28 00:08:30 -0500

    Se importo el servicio del usuario, se declaro el metodo getUsuarioque tare el id correspondiente desde la data del api

--- src/app/dashboard/usuario/usuario.component.ts
+++ src/app/dashboard/usuario/usuario.component.ts
@@ -1,4 +1,6 @@
+import { UsuarioService } from './../../services/usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-usuario',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  id:number;
+  constructor(private aRoute: ActivatedRoute,
+              private UsuarioService: UsuarioService) { 
+    this.id = +this.aRoute.snapshot.paramMap.get('id');
   }
 
+  ngOnInit(): void {
+    this.getUsuario();
+  }
+  getUsuario():void{
+    this.UsuarioService.getUsuario(this.id).subscribe(data=>{
+      console.log(data);
+      
+    })
+  }
 }
