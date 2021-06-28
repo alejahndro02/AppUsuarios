2b75b7566fe438931c75b55162758b21ea3890c9
Alejahndro <alejahndro02@gmail.com>
2021-06-27 23:31:14 -0500

    Se importo el componente usuarioComponent, se determino una ruta hija dentro del componente dashboard donde se renderizara el componente UsuarioComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 //Componentes
 import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListaUsuariosComponent } from './dashboard/lista-usuarios/lista-usuarios.component';
+import { UsuarioComponent } from './dashboard/usuario/usuario.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'layout', pathMatch:'full'},
   {path:'layout', component: LayoutComponent},
-  {path:'dashboard', component: DashboardComponent},
+  {path:'dashboard', component: DashboardComponent, children: [
+    {path:'', component: ListaUsuariosComponent},
+    {path: 'usuario/:id', component: UsuarioComponent}
+
+  ]},
   {path:'**',redirectTo: 'layout', pathMatch: 'full'}
 
 ];
